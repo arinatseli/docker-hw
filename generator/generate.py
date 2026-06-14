de591f57becec7429196f41931f072e90a608de2
Arina <arinatsel@yandex.ru>
2026-06-14 22:00:25 +0300

    Поменяла название колонок в generate

--- generator/generate.py
+++ generator/generate.py
@@ -6,15 +6,15 @@ import sys
 NUM_ROWS = 50
 
 
-COLUMNS = ["COLUMN_1", "COLUMN_2", "COLUMN_3", "COLUMN_4"]
+COLUMNS = ["ВЕС_КГ", "РОСТ_М", "ВОЗРАСТ_ЛЕТ", "КАТЕГОРИЯ"]
 
 def generate_row():
 
     return {
-        "COLUMN_1": random.randint(0, 100),
-        "COLUMN_2": round(random.uniform(1.5, 9.9), 2),
-        "COLUMN_3": random.randint(0, 100),
-        "COLUMN_4": random.choice(["A", "B", "C"]),
+        "ВЕС_КГ": random.randint(0, 100),
+        "РОСТ_М": round(random.uniform(1.5, 9.9), 2),
+        "ВОЗРАСТ_ЛЕТ": random.randint(0, 100),
+        "КАТЕГОРИЯ": random.choice(["ТРАВОЯДНОЕ", "ВСЕЯДНОЕ", "ХИЩНИК"]),
     }
 
 OUTPUT_DIR = sys.argv[1] if len(sys.argv) > 1 else "/data"
