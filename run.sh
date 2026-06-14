c8ae94e5c62c372f39a77a9999e7d55a1477d226
Arina <arinatsel@yandex.ru>
2026-06-14 22:37:04 +0300

    Добавила команды в run.sh для reporter

--- run.sh
+++ run.sh
@@ -7,7 +7,11 @@ case "$1" in
         docker run --rm -v "$(pwd)/data:/data" data-generator /data ;;
     create_local_data)
         python generator/generate.py ./data ;;
+    build_reporter)
+        docker build -t data-reporter ./reporter ;;
+    run_reporter)
+        docker run --rm -v "$(pwd)/data:/data" data-reporter ;;
     *)
-        echo "Usage: $0 {build_generator|run_generator|create_local_data}"
+        echo "Usage: $0 {build_generator|run_generator|create_local_data|build_reporter|run_reporter}"
         exit 1 ;;
 esac
