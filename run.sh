3e9f0d86bf8afd0f84963d6a2cb1745440754653
Arina <arinatsel@yandex.ru>
2026-06-14 23:05:08 +0300

    Добавила вспомогательные команды в run.sh

--- run.sh
+++ run.sh
@@ -11,7 +11,15 @@ case "$1" in
         docker build -t data-reporter ./reporter ;;
     run_reporter)
         docker run --rm -v "$(pwd)/data:/data" data-reporter ;;
+    structure)
+        ls -R ;;
+    clear_data)
+        rm -f data/*.csv data/*.html ;;
+    inside_generator)
+        docker run --rm -v "$(pwd)/data:/data" --entrypoint ls data-generator /data ;;
+    inside_reporter)
+        docker run --rm -v "$(pwd)/data:/data" --entrypoint ls data-reporter /data ;;
     *)
-        echo "Usage: $0 {build_generator|run_generator|create_local_data|build_reporter|run_reporter}"
+        echo "Usage: $0 {build_generator|run_generator|create_local_data|build_reporter|run_reporter|structure|clear_data|inside_generator|inside_reporter}"
         exit 1 ;;
 esac
